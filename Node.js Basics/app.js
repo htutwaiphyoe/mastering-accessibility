a4726e9f01e6155e84d3735881740bce9b386bf5
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-16 22:03:17 +0630

    Understanding Request & Response

--- Node.js Basics/app.js
+++ Node.js Basics/app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-//   process.exit();
+  console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node.js Basic</title></head>");
+  res.write("<body><h1>Hello, world!</h1></body>");
+  res.write("</html>");
+  //   process.exit();
+  res.end();
 });
 
 server.listen(9000);
