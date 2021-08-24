30a6f6edfd8ca36b28360cb353d82c920e642c8e
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-08-24 11:40:53 +0630

    #add: reading and writing files with fs module

--- the-complete-bootcamp/1-nodejs-fundamental/index.js
+++ the-complete-bootcamp/1-nodejs-fundamental/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+const fs = require("fs");
+
+const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(inputText);
+
+const outputText = `This is all about avocado: ${inputText}.`;
+
+fs.writeFileSync("./txt/output.txt", outputText, "utf-8");
